41e2e0632a1cec10ddcc1043b0be33ab19513c6a
Becky Howard <becky_lynne@hotmail.com>
2024-01-18 16:22:41 -0500

    second commit

---
+++ src/week5/App.java
@@ -0,0 +1,21 @@
+package week5;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Logger a = new AsteriskLogger();
+		Logger b = new SpacedLogger();
+		a.log("Lynne");
+		a.error("Eva");
+		b.log ("Karen");
+		b.error("Becky");
+		
+			
+			
+		}
+		
+	}
+
